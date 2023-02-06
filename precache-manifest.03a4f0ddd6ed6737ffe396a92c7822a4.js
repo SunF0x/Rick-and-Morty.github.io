9a1d62a581f26b85cf86769f2b16f494bfe99df8
SunF0x <buzinakatya2000@mail.ru>
2023-02-06 23:52:17 +0300

    Updates

---
+++ precache-manifest.03a4f0ddd6ed6737ffe396a92c7822a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b5e531250ad89e2a7e5",
+    "url": "/Rick-and-Morty/static/css/main.d45061ac.chunk.css"
+  },
+  {
+    "revision": "6b5e531250ad89e2a7e5",
+    "url": "/Rick-and-Morty/static/js/main.83e93abf.chunk.js"
+  },
+  {
+    "revision": "abc008415fdc7e4113c9",
+    "url": "/Rick-and-Morty/static/js/runtime~main.4e6a0c18.js"
+  },
+  {
+    "revision": "1bc543606ee5867d763f",
+    "url": "/Rick-and-Morty/static/js/2.a0491fb3.chunk.js"
+  },
+  {
+    "revision": "23ff80a49488ff4d5e788381ef4785e1",
+    "url": "/Rick-and-Morty/index.html"
+  }
+];
